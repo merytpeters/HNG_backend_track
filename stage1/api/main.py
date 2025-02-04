686618f2e9fae8f36b5308b4e09519f2671e1e72
merytpeters <merytpeters@gmail.com>
2025-02-04 18:22:35 +0100

    Dependencies updated in reqiurements.txt

--- stage1/api/main.py
+++ stage1/api/main.py
@@ -27,14 +27,14 @@ def api():
 
 
 @app.get("/api/classify-number")
-async def clasify_number(number: int = Query(..., description="Number to classify")):
+def clasify_number(number: int = Query(..., description="Number to classify")):
     """public api to display number properties"""
     try:
         number = int(number)
         is_prime = classifier.is_prime(number)
         is_perfect = classifier.is_perfect(number)
         digit_sum = classifier.digit_sum(number)
-        fun_fact = await classifier.fun_fact(number)
+        fun_fact = classifier.fun_fact(number)
         number_check = classifier.is_armstrong(number)
         numb_check = classifier.is_odd_or_even(number)
         properties = []
@@ -54,8 +54,8 @@ async def clasify_number(number: int = Query(..., description="Number to classif
             "digit_sum": digit_sum,
             "fun_fact": fun_fact
         }
-    except Exception as e:
+    except Exception:
         return {
             "number": "alphabet",
-            "error": True
+            "error": True,
         }
